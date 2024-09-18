88d5ee58b82bdc1a52930feada7770e7dffd8e1d
Mudit Mishra <153424187+Muditmishra01@users.noreply.github.com>
2024-09-18 11:00:56 +0000

    datatypes file made

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,10 +1,10 @@
 const accountId = 144553
-let accountEmail = "hitesh@google.com"
+let accountEmail = "mudit@google.com"
 var accountPassword = "12345"
 accountCity = "Jaipur"
 let accountState;
 
-// accountId = 2 // not allowed
+// accountId = 2 // this is  not allowed
 
 
 accountEmail = "hc@hc.com"
@@ -14,7 +14,7 @@ accountCity = "Bengaluru"
 console.log(accountId);
 
 /*
-Prefer not to use var
+Do not to use var
 because of issue in block scope and functional scope
 */
 
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+
+"use strict"; // treat all javascript code as new version code
+
+// alert( 3 + 3) // we are using nodejs, not browser , we do it in browser
+
+console.log(3 
+    +
+     3) // bad readibility , bad way 
+
+console.log("Hitesh")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true or flase 
+// null => standalone value 
+// undefined => 
+// symbol => represents the unique value 
+
+
+// object  -> will discuss later in future 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
