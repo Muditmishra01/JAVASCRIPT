1eb68cc3ac50767837829c84ff195e549605c086
Mudit Mishra <153424187+Muditmishra01@users.noreply.github.com>
2024-09-18 13:15:21 +0000

    datatypes summary

---
+++ 01_basics/04_comparions.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);         // these are easy comparisons 
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);         // comparison between different datatypes 
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);   // will give true because , terminal will convert the null to 0
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);  // will give false in false 
+
+// === 
+
+console.log("2" === 2);       // strict check, it not only checks the value , but also the datatypes
