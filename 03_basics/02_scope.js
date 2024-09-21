7fb769ccc6f869109d79f7638dae3a35e8f82b9f
Mudit Mishra <153424187+Muditmishra01@users.noreply.github.com>
2024-09-21 18:10:02 +0000

    scope level and mini hosting in js

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,58 @@
+//var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("INNER: ", a);
+    
+}
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username = "mudit"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);         
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "mudit"
+    if (username === "mudit") {
+        const website = " youtube"
+         //console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// +++++++++++ interesting concept about declartion of function in variable ++++++++++++++++++
+
+
+console.log(addone(5))                   // it is correct 
+
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5)                               // will give error 
+const addTwo = function(num){
+    return num + 2
+}
