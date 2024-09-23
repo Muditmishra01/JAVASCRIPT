b15f6ad0d017643873086d7cdf7e381a38513e9d
Mudit Mishra <153424187+Muditmishra01@users.noreply.github.com>
2024-09-23 06:00:14 +0000

    locale compare string function

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -19,8 +19,8 @@ console.log(gameName.indexOf('t'));
 const newString = gameName.substring(0, 4)
 console.log(newString);
 
-const anotherString = gameName.slice(-8, 4)
-console.log(anotherString);
+const anotherString = gameName.slice(-12, 4)
+console.log(anotherString);         // important
 
 const newStringOne = "   mudit    "
 console.log(newStringOne);
@@ -33,3 +33,19 @@ console.log(url.replace('%20', '-'))
 console.log(url.includes('sudeep'))
 
 console.log(gameName.split('-'));
+
+
+//EXTRA FUNCTIONS
+// 1) string.prototype.localecompare()
+
+// Case-insensitive comparison
+console.log("apple".localeCompare("Apple", undefined, { sensitivity:"base" })); // 0 (ignores case)
+
+//2) string prototype.match()
+
+let str = "The quick brown fox jumps over the quick but lazy dog";
+let result = str.match(/quick/g);   // will give list of all matches 
+
+console.log(result);
+
+//without  global g wil  give the first match
--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -32,3 +32,8 @@ const min = 10
 const max = 20
 
 console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+
+let time = Date.now()
+console.log(Math.floor(time/1000))
