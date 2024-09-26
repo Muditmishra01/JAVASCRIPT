abf0afc64630138123f61b6bb2c6f9732a129f7e
Mudit Mishra <153424187+Muditmishra01@users.noreply.github.com>
2024-09-26 17:10:37 +0000

    for in in maps

--- 05_iterations/four.js
+++ 05_iterations/four.js
@@ -17,11 +17,11 @@ const myObj = {       // for object we use for in loop
  const programming = ["js","rb","pyhton","cpp"]
 
  for (const key in programming) {
-   console.log(key)
+   console.log(programming[key])         // to get value we use the programming[key]
  }
  // in array if we are using the for in then we and print the key then we will get the (INDEX)
 
  // but as we knew that we get the direct value if using the for of loop in  the array 
- 
 
+// FOR IN in the maps -> we cannot use the for in in maps because it is not iterable through it
 
