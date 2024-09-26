c88fb1576d250482c2db757790ad694272fa599b
Mudit Mishra <153424187+Muditmishra01@users.noreply.github.com>
2024-09-26 17:07:32 +0000

    for in in array

--- 05_iterations/four.js
+++ 05_iterations/four.js
@@ -6,11 +6,22 @@ const myObj = {       // for object we use for in loop
 
 }
 
-for (const key in myObj) {
+// for (const key in myObj) {
+//    console.log(key)
+// }
+// for (const key in myObj) {
+//     console.log(`${key} shortcut is for ${myObj[key]}`)
+//  }
+
+ // can we use for in in the arrays
+ const programming = ["js","rb","pyhton","cpp"]
+
+ for (const key in programming) {
    console.log(key)
-}
-for (const key in myObj) {
-    console.log(`${key} shortcut is for ${myObj[key]}`)
  }
+ // in array if we are using the for in then we and print the key then we will get the (INDEX)
+
+ // but as we knew that we get the direct value if using the for of loop in  the array 
+ 
 
 
