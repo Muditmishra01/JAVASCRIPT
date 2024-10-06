c1627003f05959c6c57d4da3c43956bdd7c7b883
Mudit Mishra <153424187+Muditmishra01@users.noreply.github.com>
2024-10-06 11:28:46 +0000

    arrays comments

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -3,7 +3,7 @@
 const myArr = [0, 1, 2, 3, 4, 5]
 const myHeors = ["shaktiman", "naagraj"]
 
-const myArr2 = new Array(1, 2, 3, 4)
+const myArr2 = new Array(1, 2, 3, 4)     // another way of declaring
 // console.log(myArr[1]);
 
 // Array methods
@@ -12,16 +12,17 @@ const myArr2 = new Array(1, 2, 3, 4)
 // myArr.push(7)
 // myArr.pop()
 
+
 // myArr.unshift(9)         // will shift it from the starting index 
 // myArr.shift()            // will remove the shift
 
 // console.log(myArr.includes(9));
 // console.log(myArr.indexOf(3));
 
-// const newArr = myArr.join()     // converts the array into string  
+const newArr = myArr.join()     // converts the array into string  
 
-// console.log(myArr);
-// console.log( newArr);
+// console.log(myArr);        //- object 
+// console.log( newArr);      // - string 
 
 
 // slice, splice
@@ -37,3 +38,5 @@ console.log("B ", myArr);
 const myn2 = myArr.splice(1, 3)
 console.log("C ", myArr);
 console.log(myn2);
+
+
--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -6,22 +6,22 @@ const dc_heros = ["superman", "flash", "batman"]
 // console.log(marvel_heros);           
 // console.log(marvel_heros[3][1]);
 
-// const allHeros = marvel_heros.concat(dc_heros)
+// const allHeros = marvel_heros.concat(dc_heros)            // concat
 // console.log(allHeros);
 
 const all_new_heros = [...marvel_heros, ...dc_heros]       // new method to concatenate 
 
-// console.log(all_new_heros);
+ console.log(all_new_heros);
 
-const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]     // flat
 
 const real_another_array = another_array.flat(Infinity)  // to remove all the subarray
 console.log(real_another_array);
 
 
 
-console.log(Array.isArray("Mudit"))
-console.log(Array.from("Mudit"))
+console.log(Array.isArray("Mudit"))          //.isArray
+console.log(Array.from("Mudit"))              //.from
 console.log(Array.from({name: "Mudit"})) // interesting  // interview,(key-value)
 
 let score1 = 100
